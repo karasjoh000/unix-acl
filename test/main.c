940c9120ff343abcf08ee804c11c06a050967936
John Karasev <john.karasev@wsu.edu>
2018-09-23 22:53:04 -0700

    add comments

--- test/main.c
+++ test/main.c
@@ -2,8 +2,20 @@
 // Created by John Karasev on 9/21/18.
 //
 #include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+
+mode_t getMode(char *file) {
+    struct stat buf;
+    if (lstat(file, &buf) == -1) {
+        return -1;
+    }
+    return buf.st_mode;
+}
 
 int main() {
-    printf("%d", !(-1));
+    printf("%d", getMode("thisdoesnotexist"));
 }
 
