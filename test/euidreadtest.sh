cc3fa188c33499281046f4dcfcd0caffccc3c8fa
John Karasev <john.karasev@wsu.edu>
2018-09-25 21:26:32 -0700

    fix test file

--- test/euidreadtest.sh
+++ test/euidreadtest.sh
@@ -5,6 +5,7 @@ do
     sudo chmod ${i}00 /home/acluser/source.access
     ../get /home/acluser/source ../dest
 done
+sudo chmod 700 /home/acluser/source.access
 for i in `seq 0 7`
 do
     echo "checking ${i}00 mode"
